5a2fe3357ddd0d6e557ff1ab4ef4a5cb0f89e7bf
yangzian <yangzian@byrtsoft.com>
2020-01-06 18:01:43 +0800

    停止

--- src/main/java/com/chargeingpile/netty/chargeingpilenetty/controller/ChargingController.java
+++ src/main/java/com/chargeingpile/netty/chargeingpilenetty/controller/ChargingController.java
@@ -6,7 +6,9 @@ import com.chargeingpile.netty.chargeingpilenetty.service.serviceImpl.ChargingSe
 import com.chargeingpile.netty.chargeingpilenetty.shenghong.SHCmd;
 import com.chargeingpile.netty.chargeingpilenetty.shenghong.manager.ClientConnection;
 import com.chargeingpile.netty.chargeingpilenetty.shenghong.manager.ClientManager;
+import com.chargeingpile.netty.chargeingpilenetty.shenghong.message.ChargeRecordInfo;
 import com.chargeingpile.netty.chargeingpilenetty.shenghong.message.StartCharger;
+import com.chargeingpile.netty.chargeingpilenetty.shenghong.message.StopCharger;
 import com.chargeingpile.netty.chargeingpilenetty.shenghong.utils.BytesUtil;
 import com.chargeingpile.netty.chargeingpilenetty.util.ASCIIUtil;
 import com.chargeingpile.netty.chargeingpilenetty.util.CommonUtil;
@@ -73,7 +75,7 @@ public class ChargingController {
 
 
 
-    @ApiOperation(value = "预约充电/即时充电")
+    @ApiOperation(value = "预约充电/即时充电-开启")
     @PostMapping(value = "/startOrder")
     public ServerResponse startOrder() {
         try {
@@ -190,6 +192,140 @@ public class ChargingController {
                         }
                     }
 
+                }
+
+            }
+
+            return ServerResponse.createBySuccess("成功",retMap);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            return ServerResponse.createByErrorMessage("服务器异常，请联系管理员。");
+        }
+
+    }
+
+
+
+
+
+
+    @ApiOperation(value = "停止充电 或 取消预约 充电")
+    @PostMapping(value = "/stopCharge")
+    public ServerResponse stopCharge() {
+        try {
+
+
+
+            // 充电桩ID
+            String chp_ip = "169.254.151.100"; //充电桩ip
+            String usr_id = "11010001668019601101000166801960";
+
+            String chp_por = "9999"; // 充电桩端口号
+            String cha_num = "075586511588001"; // 充电桩 编号
+
+            // 0-取消预约，1-停止充电
+            String flag = "1";
+
+
+            // buwei
+            while (usr_id.length() < 64) {
+                usr_id += "0";
+            }
+
+            Map<String, Object> map = new HashMap<String, Object>();
+
+            map.put("chp_id", chp_ip);
+
+            // 查询桩ip
+
+            //
+            Map<String, Object> retMap = new HashMap<String, Object>();
+            String desc = "";
+
+            if (chp_por.equals("9999")){
+
+                CacheManager cacheManager = CacheManager.getInstance();
+
+                Cache sample = cacheManager.getCache("loginCache");
+                sample.remove(cha_num + "staChp");
+
+                Element element = new Element(cha_num + "staChp", 2);
+
+                sample.put(element);
+
+                Thread.sleep(2000);
+                if (TEST) {
+
+                    desc = "停止充电成功";
+                    retMap.put("state", 1);
+                    element = sample.get(cha_num + "retMap");
+                    Map<String, Object> map1 = (Map<String, Object>) element.getObjectValue();
+                    retMap.put("elec", map1.get("elec"));
+                    retMap.put("time", map1.get("time"));
+                    retMap.put("read_before", map1.get("read_before"));
+                    retMap.put("read_after", map1.get("read_after"));
+                } else {
+                    if (!CommonUtil.isEmpty(chp_ip)) {
+                        ClientConnection client = ClientManager.getClientConnection(chp_ip, cha_num);
+
+                        if (client == null || client.getCtx() == null) {
+                            desc = "桩未连接";
+                            retMap.put("state", 2);
+                        } else {
+                            String startAdd = "";
+                            if (flag.equals("0")) { // 预约
+                                startAdd = StopCharger.ADDR_ORDER;
+                            } else {// 即时充电
+                                startAdd = StopCharger.ADDR_CHARGE;
+                            }
+                            StopCharger charger = new StopCharger();
+                            charger.setAddr(startAdd);
+
+                            SHCmd.stopCharge(client.getCtx(), charger);
+
+                            long start = System.currentTimeMillis();
+                            while (true) {
+                                long end = System.currentTimeMillis();
+                                client = ClientManager.getClientConnection(chp_ip, cha_num);
+                                if ((end - start) > 60 * 1000) { // 大于60s，视为失败
+                                    desc = "失败-超时";
+                                    retMap.put("state", 0);
+                                    break;
+                                } else if (client.getPileState() == ClientConnection.STATE_START_FAILED) {
+                                    desc = "失败";
+                                    retMap.put("state", 0);
+                                    break;
+                                } else if ((startAdd.equals(StopCharger.ADDR_ORDER))
+                                            && (client.getPileState() == ClientConnection.STATE_NORMAL)) {
+                                    // 预约
+                                    desc = "取消预约成功";
+                                    retMap.put("state", 1);
+                                    break;
+                                } else if ((startAdd.equals(StopCharger.ADDR_CHARGE))
+                                            && (client.getPileState() == ClientConnection.STATE_NORMAL
+                                            || client.getPileState() == ClientConnection.STATE_CHARGE_OVER)) {
+                                    // 充电
+                                    desc = "停止充电成功";
+                                    retMap.put("state", 1);
+                                    ChargeRecordInfo charge = client.getChargeRecordInfo();
+                                    if (charge != null) {
+                                        double elecQua = charge.getChargeEle();
+                                        int time = charge.getDurationTime();
+                                        double read_before = charge.getChargeStart();
+                                        double read_after = charge.getChargeEnd();
+                                        retMap.put("elec", elecQua);
+                                        retMap.put("time", time);
+                                        retMap.put("read_before", read_before);
+                                        retMap.put("read_after", read_after);
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+
+                    }
                 }
 
             }
